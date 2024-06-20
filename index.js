d1b9a141468aeacd0bf4d4858389d762ec8b844f
unicorn <frienduration@gmail.com>
2024-06-20 19:01:52 +0900

    changed cors error

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(
   cors({
     origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   })
 );
 app.use(bodyParser.json());
